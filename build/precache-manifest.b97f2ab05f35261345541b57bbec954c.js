65a9703293d577057af7f9ce14fed8e13480be47
ankit554 <52959698+ankit554@users.noreply.github.com>
2020-07-10 19:19:25 +0530

    deploying front end

---
+++ build/precache-manifest.b97f2ab05f35261345541b57bbec954c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79bab451783f2d8c5cd8c27c104e6ab1",
+    "url": "/facedetection-reactapp/index.html"
+  },
+  {
+    "revision": "5740bb8846f3d2e81118",
+    "url": "/facedetection-reactapp/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "d0cfa9263fd03032885d",
+    "url": "/facedetection-reactapp/static/css/main.1743de41.chunk.css"
+  },
+  {
+    "revision": "5740bb8846f3d2e81118",
+    "url": "/facedetection-reactapp/static/js/2.e0435566.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/facedetection-reactapp/static/js/2.e0435566.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d0cfa9263fd03032885d",
+    "url": "/facedetection-reactapp/static/js/main.d3881fc4.chunk.js"
+  },
+  {
+    "revision": "cc291e0fe366982da333",
+    "url": "/facedetection-reactapp/static/js/runtime-main.1d8eb9d4.js"
+  }
+]);
